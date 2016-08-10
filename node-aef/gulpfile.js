3aad4eebbe640851ab703ca97ba01a640aee5b8a
Martijn Korteweg <martijn.korteweg@stansonhealth.com>
2016-08-10 16:55:43 -0700

    tweaks for aef

--- node-aef/gulpfile.js
+++ node-aef/gulpfile.js
@@ -4,10 +4,5 @@ var gulp = require('gulp'),
     config = require('./config/gulp/config')(),
     envConfig = require('./config/gulp/env');
 
-console.log('============ Angular 2 Starter ============');
-console.log('Current environment: ' + envConfig.ENV);
-console.log('- Change environment via --env or NODE_ENV');
-console.log('===========================================');
-
 /* Default task */
 gulp.task('default', ['serve-dev']);
